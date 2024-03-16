286311793689c592f79cad862062530d58cd1f23
nasirullahahmadi <nasir.allah.ahmadi@gmail.com>
2024-03-16 11:32:49 +0200

    first commit

---
+++ routes/Service Route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/Services Schema.jsx');
+
+// Route to fetch all items
+router.get('/', async (req, res) => {
+    try {
+        const items = await Item.find();
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Route to create a new item
+router.post('/', async (req, res) => {
+    const item = new Item({
+        service: req.body.service
+    });
+
+    try {
+        const newItem = await item.save();
+        res.status(201).json(newItem);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+module.exports = router;
